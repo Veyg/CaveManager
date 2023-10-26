7dd7b49185d06580fbbaa33198a2e5e696a5de02
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-10-26 21:13:04 +0200

    fixing the leaderboard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const sequelize = require('./database.js');
 const User = require('./models/User.js');
@@ -7,12 +7,12 @@ const ShopItem = require('./models/ShopItem.js');
 
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.MESSAGE_CONTENT,
-        Intents.FLAGS.GUILD_VOICE_STATES
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
